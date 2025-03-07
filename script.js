6d709e8510d825971e2bc34b085003972497f6ff
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-07 12:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
+	let table = document.getElementById("sampleTable");
+
+	table.innerHTML = `
+		<tr> 
+		  <td>New Cell1</td>
+		  <td>New Cell2</td>
+		</tr>
+	` + table.innerHTML
+	
   
   
 }
